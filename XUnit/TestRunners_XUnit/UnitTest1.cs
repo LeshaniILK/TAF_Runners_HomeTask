0f9f2e533f03636099464eca4be6176e28d96260
Leshani Isanka <leshaniisanka1995@gmail.com>
2022-12-08 08:54:34 +0530

    Updated with the review comments - XUnit

--- XUnit/TestRunners_XUnit/UnitTest1.cs
+++ XUnit/TestRunners_XUnit/UnitTest1.cs
@@ -1,70 +1,68 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using Xunit.Abstractions;
 
 namespace TestRunners_XUnit
 {
     public class UnitTest1 : IClassFixture<XunitSetupClass>, IDisposable
     {
+        IWebDriver driver;
         private readonly ITestOutputHelper _testOutputHelper;
+
+        public IWebElement Username => driver.FindElement(By.Name("user-name"));
+        public IWebElement Password => driver.FindElement(By.Name("password"));
+        public IWebElement LoginBtn => driver.FindElement(By.XPath("//*[@id='login-button']"));
 
 
         public UnitTest1(ITestOutputHelper testOutputHelper, XunitSetupClass XunitSetupClass)
         {
             _testOutputHelper = testOutputHelper;
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.saucedemo.com/";
             _testOutputHelper.WriteLine("Xunit test Test Precondition output");
         }
 
         [Fact]
-        public void Test_Pass()
+        public void SuccessLogin()
         {
-            string expected = "leshani";
-            string actual = "leshani";
-            Assert.Equal(expected, actual);
-        }
+            Username.SendKeys("standard_user");
+            Password.SendKeys("secret_sauce");
+            LoginBtn.Click();
 
-        [Fact]
-        public void Test_Pass2()
-        {
-            void FunctionUnderTest()
-            {
-                throw new Exception();
-            }
-
-            Action _functionUnderTest = FunctionUnderTest;
-            Assert.Throws<Exception>(() => _functionUnderTest());
-
-        }
-
-        [Theory]
-        [InlineData("First Test")]
-        [InlineData("Second Test")]
-        public void Test_Theory(string parameter)
-        {
-            _testOutputHelper.WriteLine(parameter);
+            Assert.Equal("https://www.saucedemo.com/inventory.html", driver.Url);
+            _testOutputHelper.WriteLine("Successfully Login");
         }
 
         [Fact(Skip = "Checking the skip attribute")]
         public void Test_Ignore()
         {
-            _testOutputHelper.WriteLine("Xunit test Method output");
+            _testOutputHelper.WriteLine("Xunit test Method output - Ignore Test");
         }
 
         [Fact]
         public void Test_Fail()
         {
-            Assert.Empty("Failing the test");
-        }
+            driver.Url = "https://www.saucedemo.com";
+            Username.SendKeys("standard_user");
+            Password.SendKeys("scrt_sauce");
+            LoginBtn.Click();
 
-        [Fact]
-        public void Test_Fail2()
-        {
-            string expected = "leshani";
-            string actual = "leshani123";
-            Assert.Equal(expected, actual);
+            Assert.Equal("https://www.saucedemo.com/inventory.html", driver.Url);
+            _testOutputHelper.WriteLine("Login Failed");
         }
 
         public void Dispose()
         {
             _testOutputHelper.WriteLine("Xunit test Test Postcondition output");
+
+            if (driver != null)
+            {
+                driver.Quit();
+                driver = null;
+            }
+
+            int i = 0;
         }
 
     }
